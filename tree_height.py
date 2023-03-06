cfaab5bd812be707f15bf6da96611c5c20ee2f5a
AgneseRozenberga <114361013+AgneseRozenberga@users.noreply.github.com>
2023-03-06 00:46:55 +0000

    ..................

--- tree_height.py
+++ tree_height.py
@@ -7,26 +7,25 @@ import os.path
 def compute_height(n, parents):  
     piramida ={}
     for x in range(n):
-        if parents[x] == -1:
+        piramida[x] = []
+
+    for x in range (n):
+        parent = parents[x]
+        if parent == -1:
             root = x
         else:
-            if parents[x] not in piramida:
-                piramida[parents[x]] = []
-            piramida[parents[x]].append(x)
-
+            piramida[parent].append(x)
     def augstums(nodes):
-        if nodes not in piramida:
+        if not piramida[nodes]:
             return 1
-        else:
-            return 1 + max(augstums(pamats) for pamats in piramida[nodes])
+        return 1 + max(augstums(pamats) for pamats in piramida[nodes])
     return augstums(root)
 
 def main():
     try:
-        veids = input()
-        if veids.upper == 'F':
+        veids = input().upper()
+        if veids == 'F':
             filename = input()
-            
             if 'a' in filename or 'A' in filename:
                 return
             if filename[0].isdigit():
@@ -35,7 +34,7 @@ def main():
                 with open(filename, 'r', encoding='utf-8') as file:
                     n = int(file.readline().strip())
                     parents = list(map(int, file.readline().strip().split()))
-        elif veids.upper == 'I':
+        elif veids == 'I':
             n = int(input())
             parents = list(map(int, input().split()))
         if n < 1 or n > 105:
